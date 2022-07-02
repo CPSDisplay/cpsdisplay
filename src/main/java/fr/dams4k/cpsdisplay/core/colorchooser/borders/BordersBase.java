102a66957b1c2e2bdc9fc022c381783239993727
Dams4K <dams4k@users.noreply.github.com>
2022-07-02 15:45:50 +0200

    pranked, i'm gonna make new way to add borders (in reality it's for sliders don't worry to much)

---
+++ src/main/java/fr/dams4k/cpsdisplay/core/colorchooser/borders/BordersBase.java
@@ -0,0 +1,15 @@
+package fr.dams4k.cpsdisplay.core.colorchooser.borders;
+
+import java.awt.Image;
+
+public class BordersBase {
+    private Image topLeftImage;
+    private Image topRightImage;
+    private Image bottomLeftImage;
+    private Image bottomRightImage;
+    
+    private Image leftSideImage;
+    private Image rightSideImage;
+    private Image topSideImage;
+    private Image bottomSideImage;
+}
