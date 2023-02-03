6b203231a46625ab03a50e1797557cc507b03edc
Dams4K <dams4k@users.noreply.github.com>
2023-02-03 08:50:10 +0100

    Moving References.java to core

---
+++ src/main/java/fr/dams4k/cpsdisplay/core/References.java
@@ -0,0 +1,7 @@
+package fr.dams4k.cpsdisplay.core;
+
+public class References {
+	public static final String MOD_ID = "{mod_id}";
+	public static final String MOD_NAME = "{mod_name}";
+	public static final String MOD_VERSION = "{mod_version}";
+}
