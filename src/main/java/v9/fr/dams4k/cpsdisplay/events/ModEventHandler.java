99968621218be6a5af890bc3080c4aa284f1726b
Dams4K <dams4k@users.noreply.github.com>
2023-02-15 19:49:46 +0100

    If think it's a better way to check if attack or use keybind has been pressed

--- src/main/java/v9/fr/dams4k/cpsdisplay/events/ModEventHandler.java
+++ src/main/java/v9/fr/dams4k/cpsdisplay/events/ModEventHandler.java
@@ -11,23 +11,29 @@ import fr.dams4k.cpsdisplay.proxy.ClientProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.settings.GameSettings;
+import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 
 public class ModEventHandler {
-	private List<Long> leftClicks = new ArrayList<Long>();
-	private List<Long> rightClicks = new ArrayList<Long>();
+    private boolean attackIsPressed = false;
+    private boolean useIsPressed = false;
+
+	private List<Long> attackClicks = new ArrayList<Long>();
+	private List<Long> useClicks = new ArrayList<Long>();
 
 	private GameSettings gs = Minecraft.getMinecraft().gameSettings;
 
 	@SubscribeEvent
 	public void onRenderGui(RenderGameOverlayEvent.Post gameOverlayEvent) {
 		if (gameOverlayEvent.getType() == ElementType.HOTBAR && !(Minecraft.getMinecraft().currentScreen instanceof GuiIngameMenu)) {
-			new GuiOverlay(Minecraft.getMinecraft(), this.getLeftCPS(), this.getRightCPS());
+			new GuiOverlay(Minecraft.getMinecraft(), this.getAttackCPS(), this.getUseCPS());
 		}
 	}
 	
@@ -38,26 +44,45 @@ public class ModEventHandler {
 		}
 	}
 
-	@SubscribeEvent
-	public void onKeyPress(InputEvent.MouseInputEvent event) {
-		if (Mouse.getEventButtonState()) {
-			if (gs.keyBindAttack.isKeyDown() && Mouse.getEventButton() == gs.keyBindAttack.getKeyCode()+100)
-				leftClicks.add(System.currentTimeMillis());
-			
-			if (gs.keyBindUseItem.isKeyDown() && Mouse.getEventButton() == gs.keyBindUseItem.getKeyCode()+100)
-				rightClicks.add(System.currentTimeMillis());
-		}
+    @SubscribeEvent
+    public void onMouseEvent(MouseEvent e) {
+        this.onKeyPress();
+    }
+
+    @SubscribeEvent
+    public void onKeyboardEvent(KeyInputEvent e) {
+        this.onKeyPress();
+    }
+
+	public void onKeyPress() {
+        if (gs.keyBindAttack.isKeyDown()) {
+            if (!attackIsPressed) {
+                attackIsPressed = true;
+                attackClicks.add(System.currentTimeMillis());
+            }
+        } else {
+            attackIsPressed = false;
+        }
+        
+        if (gs.keyBindUseItem.isKeyDown()) {
+            if (!useIsPressed) {
+                useIsPressed = true;
+                useClicks.add(System.currentTimeMillis());
+            }
+        } else {
+            useIsPressed = false;
+        }
 	}
 
-	public int getLeftCPS() {
+	public int getAttackCPS() {
 		long currentTime = System.currentTimeMillis();
-		this.leftClicks.removeIf(e -> (e.longValue() + 1000l < currentTime));
-		return leftClicks.size();
+		this.attackClicks.removeIf(e -> (e.longValue() + 1000l < currentTime));
+		return attackClicks.size();
 	}
 
-	public int getRightCPS() {
+	public int getUseCPS() {
 		long currentTime = System.currentTimeMillis();
-		this.rightClicks.removeIf(e -> (e.longValue() + 1000l < currentTime));
-		return rightClicks.size();
+		this.useClicks.removeIf(e -> (e.longValue() + 1000l < currentTime));
+		return useClicks.size();
 	}
 }
