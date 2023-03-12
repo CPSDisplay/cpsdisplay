f1e1cb625aba895f81d698905f7d49baa2c3c2a4
Dams4K <dams4k@users.noreply.github.com>
2023-03-12 15:57:43 +0100

    fix weird error

--- mod_build.sh
+++ mod_build.sh
@@ -1,5 +1,15 @@
 # Just cleaning
-rm -rf bin/
-rm -rf build/!("libs")
+BIN_DIR="bin"
+BUILD_DIR="build"
+
+if [ -d "$BIN_DIR" ];
+then
+    rm -rf bin/
+fi
+
+if [ -d "$BUILD_DIR" ];
+then
+    rm -rf build/"!(libs)"
+fi
 
 ./gradlew --build-file build.$1.gradle build
