68695e22ebb58299ff6db9db226fc98e4f557af9
Dams4K <dams4k@users.noreply.github.com>
2023-04-11 18:09:56 +0200

    repositionate

--- moving-cards.js
+++ moving-cards.js
@@ -10,8 +10,12 @@ for (const element of elements) {
         let degY = (x * maxDeg) / (element.offsetWidth/2);
         element.style.transform = "perspective(100em) rotateX(" + degX + "deg) rotateY(" + degY + "deg)";
     });
-}
-
-function onHover() {
-
+    element.addEventListener("mouseout", (event) => {
+        var an = element.animate({ transform: "perspective(100em) rotateX(0deg) rotateY(0deg)" },
+            200, "ease-in", function() {console.log("ended");}
+        )
+        an.onfinish = function(event) {
+            element.style.transform = "perspective(100em) rotateX(0deg) rotateY(0deg)";
+        }
+    });
 }
