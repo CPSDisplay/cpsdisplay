d184528f1a711380508998b843ceadbf6de585dc
dams4k <dams4k@users.noreply.github.com>
2023-05-22 20:01:55 +0200

    specific game version

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,30 +1,27 @@
 from discord import *
 
 from utils.bot_embeds import InformativeEmbed
-from utils.mod_data import ModData, CurseForgeData
+from utils.mod_data import GithubData, ModrinthData, CurseForgeData
 
 
 class StatsCog(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @Cog.listener()
-    async def on_ready(self):
-        await CurseForgeData.get_files()
-
     @slash_command(
         name="downloads",
         name_localizations={"fr": "téléchargements"}
     )
-    @option("mod_version", choices=ModData.get_mod_versions())
-    async def downloads_command(self, ctx, mod_version: str):
+    # @option("mod_version", choices=ModData.get_mod_versions(), default=None)
+    @option("game_version", choices=GithubData.get_game_versions(), default=None)
+    async def downloads_command(self, ctx, game_version = None):
         await ctx.defer()
 
-        github_downloads = await ModData.get_github_downloads()
-        modrinth_downloads = await ModData.get_modrinth_downloads()
-        curseforge_downloads = await ModData.get_curseforge_downloads()
+        curseforge_downloads = await CurseForgeData.get_downloads(game_version=game_version or "*")
+        modrinth_downloads = await ModrinthData.get_downloads(game_version=game_version or "*")
+        github_downloads = await GithubData.get_downloads(game_version=game_version or "*")
 
-        total_downloads = max(0, github_downloads) + max(0, modrinth_downloads) + max(0, curseforge_downloads)
+        total_downloads = github_downloads + modrinth_downloads + curseforge_downloads
 
         embed = InformativeEmbed(title="Mod Downloads", description=f"**Total: {total_downloads}**")
         embed.add_field(name="Curseforge", value=f"{curseforge_downloads} downloads")
--- utils/mod_data.py
+++ utils/mod_data.py
@@ -1,68 +1,121 @@
+import json
+from distutils.version import StrictVersion
 from pprint import pprint
-from urllib.parse import urljoin
+from urllib.parse import (ParseResult, parse_qsl, unquote, urlencode, urljoin,
+                          urlparse)
 
 import aiohttp
 from requests import get
 
 from utils.references import References
 
+class SafeDict(dict):
+    def __missing__(self, key):
+        return "{" + key + "}"
 
-class ModDataVersion:
-    LATEST = 0
+class EndPoints:
+    def add_param(self, url, params):
+        url = unquote(url)
+        parsed_url = urlparse(url)
+        get_args = parsed_url.query
+        parsed_get_args = dict(parse_qsl(get_args))
+        parsed_get_args.update(params)
+        
+        # convert what needs to be converted to str
+        parsed_get_args.update(
+            {k: json.dumps(v) for k, v in parsed_get_args.items()
+            if isinstance(v, (bool, dict, list, tuple))}
+        )
+        
+        encoded_get_args = urlencode(parsed_get_args, doseq=True)
+        new_url = ParseResult(
+            parsed_url.scheme, parsed_url.netloc, parsed_url.path,
+            parsed_url.params, encoded_get_args, parsed_url.fragment
+        ).geturl()
+        return new_url
 
-class _ModData:
-    GITHUB_RELEASES_URL = "https://api.github.com/repos/dams4k/minecraft-cpsdisplay/releases"
-    GITHUB_TAGS_URL = "https://api.github.com/repos/dams4k/minecraft-cpsdisplay/tags"
+class GithubEndPoints(EndPoints):
+    LASTEST_RELEASE = "repos/{username}/{repo}/releases/latest"
+    RELEASE_BY_TAG = "/repos/{username}/{repo}/releases/tags/{tag}"
+    RELEASES = "repos/{username}/{repo}/releases"
+    TAGS = "repos/{username}/{repo}/tags"
+
+    def __init__(self):
+        for attr in GithubEndPoints.__dict__:
+            value = GithubEndPoints.__dict__[attr]
+            if isinstance(value, str):
+                value = value.format_map(SafeDict(username=References.GITHUB_USERNAME, repo=References.GITHUB_REPO))
+                setattr(GithubEndPoints, attr, value)
+
+class _GithubData:
+    BASE_URL: str = "https://api.github.com/"
+    END_POINTS = GithubEndPoints()
+
     MODRINTH_URL = "https://api.modrinth.com/v2/project/cpsdisplay"
     CURSEFORGE_URL = "https://www.curseforge.com/api/v1/mods/search?gameId=432&index=0&classId=6&filterText=cpsdisplay&gameVersion=1.8.9&pageSize=20&sortField=1&categoryIds%5B0%5D=424&categoryIds%5B1%5D=423&gameFlavors%5B0%5D=1"
 
+    @property
+    def headers(self):
+        return {
+            "Content-Type": "application/json"
+        }
+
+    def get_url(self, url: str):
+        return urljoin(self.BASE_URL, url)
+
     def get_mod_versions(self) -> list:
-        tags = get(_ModData.GITHUB_TAGS_URL).json()
+        tags = get(self.get_url(self.END_POINTS.TAGS), headers=self.headers).json()
         return [tag["name"] for tag in tags]
 
+    def get_game_versions(self) -> list:
+        game_versions = []
+        for asset in get(self.get_url(self.END_POINTS.LASTEST_RELEASE), headers=self.headers).json().get("assets", []):
+            name = asset["name"]
+            if "sources" in name:
+                continue
+            parts = name.replace("jar", "").split("-")
+            game_versions.append(parts[1].replace("mc", ""))
+
+        return sorted(game_versions, key=StrictVersion)
+
     async def get_data(self, url: str) -> dict:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers=self.headers) as session:
             async with session.get(url) as response:
                 if response.status == 200:
                     return await response.json()
 
-    async def get_curseforge_data(self) -> dict:
-        if mods_data := (await self.get_data(_ModData.CURSEFORGE_URL)).get("data", []):
-            for mod_data in mods_data:
-                if mod_data.get("id") == References.CURSEFORGE_MOD_ID:
-                    return mod_data
-        return {}
-
-    async def get_github_downloads(self) -> int:
-        downloads = -1
-        if releases := await self.get_data(_ModData.GITHUB_RELEASES_URL):
-            downloads = 0
+    async def get_assets(self, game_version: str = "*") -> dict:
+        assets = []
+        if releases := await self.get_data(self.get_url(self.END_POINTS.RELEASES)):
             for release in releases:
-                downloads += sum([asset.get("download_count", 0) for asset in release.get("assets", []) if not "source" in asset.get("name", "")])
-        
-        return downloads
+                for asset in release.get("assets"):
+                    name = asset["name"]
 
-    async def get_modrinth_downloads(self) -> int:
-        downloads = -1
-        if data := await self.get_data(_ModData.MODRINTH_URL):
-            downloads = data.get("downloads", -1)
+                    if "sources" in name:
+                        continue
+                    if game_version != "*" and not game_version in name:
+                        continue
+                    
+                    assets.append(asset)
 
-        return downloads
+        return assets
 
-    async def get_curseforge_downloads(self) -> int:
-        downloads = -1
-        if data := await self.get_curseforge_data():
-            downloads = data.get("downloads", -1)
+    async def get_downloads(self, game_version: str = "*") -> int:
+        assets = await self.get_assets(game_version)
+        return sum([asset.get("download_count", 0) for asset in assets])
 
-        return downloads
 
-ModData = _ModData()
+GithubData = _GithubData()
+
+class CurseForgeEndPoints(EndPoints):
+    GET_FILES = "/v1/mods/%s/files" % References.CURSEFORGE_MOD_ID
+
+    def game_version(self, url, version): 
+        return self.add_param(url, {"gameVersion": version})
 
 class _CurseForgeData:
     BASE_URL = "https://api.curseforge.com/"
-
-    class EndPoint:
-        GET_FILES = "/v1/mods/%s/files" % References.CURSEFORGE_MOD_ID
+    END_POINTS = CurseForgeEndPoints()
 
     def __init__(self):
         self.api_key = References.CURSEFORGE_API_KEY
@@ -71,11 +124,11 @@ class _CurseForgeData:
     def headers(self):
         return {
             "Accept": "application/json",
-            "x-api-key": "$2a$10$yNvq9o/x2EbNhmuxFeM6XuHa62zEcj8JbaM5LQwXagFadzm9IsVGW"
+            "x-api-key": self.api_key
         }
 
     def get_url(self, url: str):
-        return urljoin(_CurseForgeData.BASE_URL, url)
+        return urljoin(self.BASE_URL, url)
 
     async def get_data(self, url) -> dict:
         async with aiohttp.ClientSession(headers=self.headers) as session:
@@ -84,10 +137,62 @@ class _CurseForgeData:
                     return await response.json()
         return {}
     
-    async def get_files(self):
-        url = self.get_url(_CurseForgeData.EndPoint.GET_FILES)
+    async def get_files(self, game_version: str = "*"):
+        url = self.get_url(self.END_POINTS.GET_FILES)
+        if game_version != "*":
+            url = self.END_POINTS.game_version(url, game_version)
         
-        files = await self.get_data(url)
-        pprint(files)
+        data = await self.get_data(url)
+        return data["data"]
+
+    async def get_downloads(self, game_version: str = "*") -> int:
+        files = await self.get_files(game_version)
+        return sum([file.get("downloadCount", 0) for file in files])
+
 
 CurseForgeData = _CurseForgeData()
+
+class ModrinthEndPoints(EndPoints):
+    GET_VERSION = "v2/project/%s/version" % References.MODRINTH_MODSLUG
+
+    def game_version(self, url, *args):
+        return self.add_param(url, {"game_versions": args})
+
+class _ModrinthData():
+    BASE_URL = "https://api.modrinth.com/"
+    END_POINTS = ModrinthEndPoints()
+
+    @property
+    def headers(self):
+        return {
+            "Accept": "application/json",
+            "User-Agent": "dams4k/minecraft-cpsdisplay"
+        }
+    
+    def get_url(self, url: str):
+        return urljoin(self.BASE_URL, url)
+
+    async def get_data(self, url) -> dict:
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    return await response.json()
+        return {}
+
+    async def get_files(self, game_version: str="*") -> dict:
+        url = self.get_url(self.END_POINTS.GET_VERSION)
+        if game_version != "*":
+            url = self.END_POINTS.game_version(url, game_version)
+        
+        files = await self.get_data(url)
+        
+        if isinstance(files, list):
+            return files
+        return []
+    
+    async def get_downloads(self, game_version: str = "*") -> int:
+        files = await self.get_files(game_version)
+        return sum([file.get("downloads", 0) for file in files])
+
+
+ModrinthData = _ModrinthData()
