fdb2087573a50dbe28ef2e7280cc3eac5a09b679
Dams4K <dams4k@users.noreply.github.com>
2023-10-21 18:55:38 +0200

    here we go

--- src/main/java/core/fr/dams4k/cpsdisplay/References.java
+++ src/main/java/core/fr/dams4k/cpsdisplay/References.java
@@ -3,6 +3,6 @@ package fr.dams4k.cpsdisplay;
 public class References {
 	public static final String MOD_ID = "cpsdisplay";
 	public static final String MOD_NAME = "CPSDisplay";
-	public static final String MOD_VERSION = "2.2.1";
+	public static final String MOD_VERSION = "2.2.2";
     public static final String MOD_GITHUB_LASTEST_RELEASE = "https://api.github.com/repos/CPSDisplay/cpsdisplay/releases/latest";
 }
